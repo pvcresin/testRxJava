5eef2aaf2c95fd25034774b0a65d5a054eda24cb
pvcresin <pvcresin0730@gmail.com>
2018-02-18 16:20:31 +0900

    init

---
+++ src/main/java/rxjava/HelloWorld.java
@@ -0,0 +1,10 @@
+package rxjava;
+
+import io.reactivex.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Flowable.just("Hello world")
+                .subscribe(System.out::println);
+    }
+}

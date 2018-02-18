f48c822630f93d1616a390a0d3133d7e66d1352a
pvcresin <pvcresin0730@gmail.com>
2018-02-18 16:50:12 +0900

    RxJava github

--- src/main/java/rxjava/HelloWorld.java
+++ src/main/java/rxjava/HelloWorld.java
@@ -1,10 +1,42 @@
 package rxjava;
 
-import io.reactivex.*;
+import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
 
 public class HelloWorld {
+
     public static void main(String[] args) {
-        Flowable.just("Hello world")
-                .subscribe(System.out::println);
+        Flowable<String> source = Flowable.fromCallable(() -> {
+            Thread.sleep(1000); //  imitate expensive computation
+            return "Done";
+        });
+
+        Flowable<String> runBackground = source.subscribeOn(Schedulers.io());
+
+        Flowable<String> showForeground = runBackground.observeOn(Schedulers.single());
+
+        showForeground.subscribe(System.out::println, Throwable::printStackTrace);
+
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\nsequential");
+
+        Flowable.range(1, 10)
+                .observeOn(Schedulers.computation())
+                .map(v -> v * v)
+                .blockingSubscribe(System.out::println);
+
+        System.out.println("\nparallel");
+
+        Flowable.range(1, 10)
+                .parallel()
+                .runOn(Schedulers.computation())
+                .map(v -> v * v)
+                .sequential()
+                .blockingSubscribe(System.out::println);
     }
 }
